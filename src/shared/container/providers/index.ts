114009d0aa07102fc7aa046cec5d8c26cb9ad86d
Douglas Lisboa <douglas.lisboa@edu.unipar.br>
2021-03-05 08:52:17 -0300

    Ajuste singleton providers e-mail, EtherrelaMailProvider deve ser executado após o HandlebarsMailTemplateProvider.

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,12 +6,20 @@ import DiskStorageProvider  from './StorageProvider/implementations/DiskStorageP
 import IMailProvider from './MailProvider/Models/IMailProvider';
 import EtherealMailProvider  from './MailProvider/Implementations/EtherealMailProvider';
 
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider  from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider> (
     'StorageProvider',
     DiskStorageProvider,
 );
 
+container.registerSingleton<IMailTemplateProvider> (
+    'MailTemplateProvider',
+    HandlebarsMailTemplateProvider,
+);
+
 container.registerInstance<IMailProvider> (
     'MailProvider',
-    new EtherealMailProvider(),
+    container.resolve(EtherealMailProvider),
 );
